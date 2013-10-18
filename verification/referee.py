b310fb508c4ef67fb5f54eac1087ee4772476330
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-18 21:36:29 +1200

    images

--- verification/referee.py
+++ verification/referee.py
@@ -1,8 +1,6 @@
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
-from checkio.referees import cover_codes
-from checkio.referees import checkers
 
 from tests import TESTS
 
