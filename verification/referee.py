6956de4c3f5461f25185b523500143cb4f0ff7bb
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-18 21:35:30 +1200

    referee and tests

--- verification/referee.py
+++ verification/referee.py
@@ -1,31 +1,3 @@
-"""
-CheckiOReferee is a base referee for checking you code.
-    arguments:
-        tests -- the dict contains tests in the specific structure.
-            You can find an example in tests.py.
-        cover_code -- is a wrapper for the user function and additional operations before give data
-            in the user function. You can use some predefined codes from checkio.referee.cover_codes
-        checker -- is replacement for the default checking of an user function result. If given, then
-            instead simple "==" will be using the checker function which return tuple with result
-            (false or true) and some additional info (some message).
-            You can use some predefined codes from checkio.referee.checkers
-        add_allowed_modules -- additional module which will be allowed for your task.
-        add_close_builtins -- some closed builtin words, as example, if you want, you can close "eval"
-        remove_allowed_modules -- close standard library modules, as example "math"
-
-checkio.referee.checkers
-    checkers.float_comparison -- Checking function fabric for check result with float numbers.
-        Syntax: checkers.float_comparison(digits) -- where "digits" is a quantity of significant
-            digits after coma.
-
-checkio.referee.cover_codes
-    cover_codes.unwrap_args -- Your "input" from test can be given as a list. if you want unwrap this
-        before user function calling, then using this function. For example: if your test's input
-        is [2, 2] and you use this cover_code, then user function will be called as checkio(2, 2)
-    cover_codes.unwrap_kwargs -- the same as unwrap_kwargs, but unwrap dict.
-
-"""
-
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
@@ -34,16 +6,39 @@ from checkio.referees import checkers
 
 from tests import TESTS
 
+GOOD_ACTIONS = ("12", "10", "01", "02", "20", "21")
+
+
+def process_data(answer, user_result):
+    min_length, indata = answer
+    start, end = indata.split("-")
+    stacks = [[], list(start), []]
+    if not isinstance(user_result, str):
+        return False, ("", "Your result is not a string.")
+    actions = user_result.split(",")
+    user_actions = []
+    for act in actions:
+        if act not in GOOD_ACTIONS:
+            return False, (",".join(user_actions), "Wrong action " + str(act))
+        from_s = int(act[0])
+        to_s = int(act[1])
+        if not stacks[from_s]:
+            return False, (",".join(user_actions), "Tried to pop from empty stack")
+        if to_s == 0 and stacks[to_s]:
+            return False, (",".join(user_actions), "Tried to push in the full buffer")
+        stacks[to_s].append(stacks[from_s].pop())
+        user_actions.append(act)
+    res_word = ''.join(stacks[2])
+    if len(actions) > min_length:
+        return False, (",".join(user_actions), "Too long solution.")
+    if res_word == end:
+        return True, (",".join(user_actions), "All right!")
+    else:
+        return False, (",".join(user_actions), "Wrong word in the result.")
+
+
 api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
-        cover_code={
-            'python-27': cover_codes.unwrap_args,  # or None
-            'python-3': cover_codes.unwrap_args
-        },
-        # checker=None,  # checkers.float.comparison(2)
-        # add_allowed_modules=[],
-        # add_close_builtins=[],
-        # remove_allowed_modules=[]
-    ).on_ready)
+        checker=process_data).on_ready)
--- verification/tests.py
+++ verification/tests.py
@@ -3,34 +3,119 @@ TESTS is a dict with all you tests.
 Keys for this will be categories' names.
 Each test is dict with
     "input" -- input data for user function
-    "answer" -- your right answer
-    "explanation" -- not necessary key, it's using for additional info in animation.
+    "explanation" -- your right explanation
+    "answer" -- not necessary key, it's using for additional info in animation.
 """
 
 
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": "rice-cire",
+            "explanation": "10,12,12,12,02",
+            "answer": 5
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
+            "input": "tort-trot",
+            "explanation": "12,12,12,12",
+            "answer": 4
+        },
+        {
+            "input": "hello-holle",
+            "explanation": "12,12,12,12,10,21,21,21,21,02,12,12,12,12",
+            "answer": 14
+        },
+        {
+            "input": "anagram-mragana",
+            "explanation": "12,10,12,02,12,12,12,12",
+            "answer": 8
+        },
+        {
+            "input": "mirror-mirorr",
+            "explanation": "12,12,10,12,12,02,10,21,21,21,21,21,02,12,12,12,10,21,21,21,02,12,12,12,12",
+            "answer": 25
+        },
+        {
+            "input": "arrrrrgh-ghrrrrra",
+            "explanation": "10,12,02,12,12,12,12,12,12",
+            "answer": 9
+        },
+        {
+            "input": "checkio-iocheck",
+            "explanation": "10,12,02,10,12,02,10,12,01,21,20,12,12,12,02",
+            "answer": 15
+        },
+        {
+            "input": "windows-downsiw",
+            "explanation": "12,10,12,02,10,21,21,01,20,12,12,12,12,02,12,12",
+            "answer": 16
+        },
+        {
+            "input": "tests-setts",
+            "explanation": "12,10,12,12,01,21,20,12,12,12,02",
+            "answer": 11
+        },
+        {
+            "input": "solution-tionulos",
+            "explanation": "10,12,12,02,10,21,21,01,20,12,12,02,12,12,12,12,12",
+            "answer": 17
+        },
+        {
+            "input": "superman-namperus",
+            "explanation": "12,12,12,10,12,02,10,21,21,02,12,12,12,12",
+            "answer": 14
+        },
+        {
+            "input": "library-rarybil",
+            "explanation": "10,12,12,12,02,12,12,12",
+            "answer": 8
         }
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": "arb-bra",
+            "explanation": "12,12,12",
+            "answer": 3
         },
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
+            "input": "qwerty-ytreqw",
+            "explanation": "12,12,12,12,10,12,02",
+            "answer": 7
+        },
+        {
+            "input": "rest-ters",
+            "explanation": "12,10,12,12,02",
+            "answer": 5
+        },
+        {
+            "input": "stack-ckast",
+            "explanation": "10,12,02,12,10,12,02",
+            "answer": 7
+        },
+        {
+            "input": "list-silt",
+            "explanation": "10,12,12,12,02",
+            "answer": 5
+        },
+        {
+            "input": "offroad-roadffo",
+            "explanation": "10,12,12,02,10,21,21,01,20,12,12,02,12,12,12,12",
+            "answer": 16
+        },
+        {
+            "input": "youmean-nameyuo",
+            "explanation": "12,12,10,12,02,12,12,10,21,21,02,12,12",
+            "answer": 13
+        },
+        {
+            "input": "roadmap-padroam",
+            "explanation": "12,12,10,12,12,12,02,10,21,21,01,20,12,12,02,12",
+            "answer": 16
         }
     ]
 }
+
+
+for cat in TESTS.keys():
+    for t in TESTS[cat]:
+        t["answer"] = t["answer"], t["input"]
